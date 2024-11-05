2a14aa8ab520415b7838d8f0f842ebbcf81af78a
TheFox-cmd <frover997@gmail.com>
2024-11-05 13:50:49 -0600

    Add pathname state management for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/profile/profile'
 import Contact from './components/contacts/contact'; 
+import { useEffect, useState } from 'react';
 // import NotFound from './NotFound'; // Optional: Not found component
 
 function App() {
+  const [path, setPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setPath(window.location.pathname);
+    }
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    }
+  }, []);
 
   return (
     <Router>
